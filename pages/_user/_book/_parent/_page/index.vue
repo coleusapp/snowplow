43a475fb53510803f3f63e9e61d79a03bbe87a09
Amirmasoud <amirmasood33@gmail.com>
2020-11-12 16:49:12 +0330

    Basic structure

--- pages/_user/_book/_parent/_page/index.vue
+++ pages/_user/_book/_parent/_page/index.vue
@@ -1,3 +1,24 @@
 <template>
-    <p>hello page</p>
+  <div class="container">
+    <h1>content:</h1>
+    <ul>
+      <li v-for="content of contents" :key="content.order">
+        <pre>{{ content.m1 }}</pre>
+        <pre>{{ content.m2 }}</pre>
+        <hr />
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const contents = await $content(`books/${params.user}/${params.book}/${params.parent}/sh${params.page}`).sortBy('order').fetch()
+
+      return {
+        contents
+      }
+    }
+  }
+</script>

2131adef1a5f2d87008d059937f780d5abe6b554
amirmasoud <amirmasoud@users.noreply.github.com>
2020-11-14 01:40:10 +0000

    deploy: f78701a1e7314656ebce597db239ff0fa1a56bf6

---
+++ _nuxt/static/1605317984/مولانا/مثنوی-معنوی/دفتر-۱/1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1605317984",layout:"default",error:null,serverRendered:true,routePath:"\u002Fمولانا\u002Fمثنوی-معنوی\u002Fدفتر-۱\u002F1",config:{appLocale:"fa",baseUrl:"http:\u002F\u002Flocalhost:3000",content:{dbHash:"bd5f222a"}}};

473bfd58c6dbcb81ad070c28626942fd65e6ddc2
amirmasoud <amirmasoud@users.noreply.github.com>
2020-11-14 02:16:55 +0000

    deploy: b4f1527b7c0ccdc4788794515259c3fc1917d0d9

---
+++ _nuxt/static/1605320192/فردوسی/شاهنامه/آغاز/1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1605320192",layout:"default",error:null,serverRendered:true,routePath:"\u002Fفردوسی\u002Fشاهنامه\u002Fآغاز\u002F1",config:{appLocale:"fa",baseUrl:"http:\u002F\u002Flocalhost:3000",content:{dbHash:"b760d3c6"}}};
---
+++ _nuxt/static/1605320192/محمداقبال/اسرار-خودی/اسرار-خودی/1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1605320192",layout:"default",error:null,serverRendered:true,routePath:"\u002Fمحمداقبال\u002Fاسرار-خودی\u002Fاسرار-خودی\u002F1",config:{appLocale:"fa",baseUrl:"http:\u002F\u002Flocalhost:3000",content:{dbHash:"b760d3c6"}}};

f78701a1e7314656ebce597db239ff0fa1a56bf6
Amirmasoud <amirmasood33@gmail.com>
2020-11-14 05:07:44 +0330

    Fix link

--- pages/_user/index.vue
+++ pages/_user/index.vue
@@ -34,18 +34,7 @@
           :key="book.slug"
           class="p-2 w-1/2 md:w-1/3 lg:w-1/6"
         >
-          <nuxt-link
-            :to="{
-              name: 'user-book-parent-page',
-              params: {
-                username: user.slug,
-                book: book.slug,
-                parent: book.parent,
-                page: 1
-              },
-              query: { page: 1 }
-            }"
-          >
+          <nuxt-link :to="book.open_book">
             <coleus-book-card :user="user" :book="book" class="mx-auto" />
           </nuxt-link>
         </div>
@@ -70,7 +59,6 @@ export default {
     user = user.length ? user[0] : null
 
     const books = []
-    const parent = 'parent'
     const book = await $content(`books/${user.slug}/books`)
       .sortBy('order')
       .fetch()
@@ -81,20 +69,18 @@ export default {
         )
           .where({ order: 0 })
           .fetch()
-        book[j].open_book = `books/${user.slug}/${book[j].slug}/${
+        book[j].open_book = `${user.slug}/${book[j].slug}/${
           firstPage.length ? firstPage[0].slug : firstPage.slug
         }/1`
-        book[parent] = firstPage.length ? firstPage[0].slug : firstPage.slug
         books.push(book[j])
       }
     } else {
       const firstPage = await $content(`books/${user.slug}/${book.slug}/pages`)
         .where({ order: 0 })
         .fetch()
-      book.open_book = `books/${user.slug}/${book.slug}/${
+      book.open_book = `${user.slug}/${book.slug}/${
         firstPage.length ? firstPage[0].slug : firstPage.slug
       }/1`
-      book[parent] = firstPage.length ? firstPage[0].slug : firstPage.slug
       books.push(book)
     }
 

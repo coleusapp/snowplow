6e4e41d9c1741b8356c991c0a005e55ba1d3d2ca
amirmasoud <amirmasoud@users.noreply.github.com>
2020-11-14 03:16:29 +0000

    deploy: 5ff03a2182b9b97c58941952c7fb0f7d99f1d317

--- _nuxt/static/1605323596/محمداقبال/پس-چه-باید-کرد؟/پس-چه-باید-کرد؟/1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1605323596",layout:"default",error:null,serverRendered:true,routePath:"\u002Fمحمداقبال\u002Fپس-چه-باید-کرد؟\u002Fپس-چه-باید-کرد؟\u002F1",config:{appLocale:"fa",baseUrl:"http:\u002F\u002Flocalhost:3000",content:{dbHash:"7acbebc8"}}};

93e288d4bc19a43972e9327e857697ad6d125860
amirmasoud <amirmasoud@users.noreply.github.com>
2020-11-14 23:46:10 +0000

    deploy: 3db41d9fdf2f4c8aa7017a1ba53df5c2922e9d95

--- _nuxt/static/1605323767/پروین‌اعتصامی/دیوان-اشعار/قصاید/1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1605323767",layout:"default",error:null,serverRendered:true,routePath:"\u002Fپروین‌اعتصامی\u002Fدیوان-اشعار\u002Fقصاید\u002F1",config:{appLocale:"fa",baseUrl:"http:\u002F\u002Flocalhost:3000",content:{dbHash:"12bc930c"}}};
